d3eb7e5bc12e7219617b2a16e910ad696cd296f6
NutMontree <nutmontree29@gmail.com>
2025-08-26 16:13:00 +0700

    'v1'

---
+++ src/app/api/navlink.tsx
@@ -0,0 +1,15 @@
+import { NavLinks } from '@/types/navlink'
+
+export const navLinks: NavLinks[] = [
+  { label: 'Home', href: '/' },
+  { label: 'หมวดหมู่', href: '/properties' },
+  { label: 'บล็อก', href: '/blogs' },
+  { label: 'Contact', href: '/contactus' },
+  { label: 'Docs', href: '/documentation' },
+
+  { label: 'บริการ', href: '/Services' },
+  { label: 'ผลงาน', href: '/Works' },
+  { label: 'การประมาณราคา', href: '/CosntEstiimation' },
+  { label: 'คำศัพท์', href: '/Glossary' },
+  { label: 'เกี่ยวกับ', href: '/About' },
+]
